6806359b2d1cd86db98e53407a5cafda399193a7
Carlton Mpofu <G17M7744@ict.ru.ac.za>
2020-02-12 21:34:54 +0200

    Carlton done

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -13,10 +13,22 @@ namespace Euler
         {
             return arry_Values.Sum();
         }
+		
+		static int findMax(int a,int b){
+			if(a > b)
+				return a;
+			else
+				return b;
+		}
+		
+		
         static void Main(string[] args)
         {
             int[] Arry = new int[] { 1, 2, 5, 9, 8, 5, 5, 6, 58, 55, 5, 54, 58, 5, 58, 5, 5, };
             Console.Write(SumArry(Arry));
+			
+			Console.Write(findMax(5,10));
+			
             Console.Read();
         }
     }
